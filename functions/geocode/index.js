c98a84af9b7cdc489d65fd143e9f16fbee1e7dd2
Joël Thierry <ntebej@gmail.com>
2021-11-14 03:32:56 +0000

    Feat - Integrating Places API and Geocode API from google

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,11 +1,30 @@
+const functions = require("firebase-functions");
 const url = require("url");
 
 const { locations: locationsMock } = require("./geocode.mock");
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
 
-  const locationMock = locationsMock[city.toLowerCase()];
+  if (mock === "true") {
+    const locationMock = locationsMock[city.toLowerCase()];
 
-  response.json(locationMock);
+    return response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: functions.config().geocodeplaces.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
--- functions/places/index.js
+++ functions/places/index.js
@@ -1,14 +1,38 @@
+const functions = require("firebase-functions");
 const url = require("url");
 
 const { mocks, addMockImage } = require("./mock");
 
-module.exports.placesRequest = (request, response) => {
-  const { location } = url.parse(request.url, true).query;
-  const data = mocks[location];
+module.exports.placesRequest = (request, response, client) => {
+  const { location, mock } = url.parse(request.url, true).query;
 
-  if (data) {
-    data.results = data.results.map(addMockImage);
+  if (mock === "true") {
+    const data = mocks[location];
+
+    if (data) {
+      data.results = data.results.map(addMockImage);
+    }
+
+    return response.json(data);
   }
 
-  response.json(data);
+  client
+    .placesNearby({
+      params: {
+        location,
+        radius: 1500,
+        type: "restaurant",
+        key: functions.config().geocodeplaces.key,
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      res.data.results = res.data.results.map(addMockImage);
+
+      return response.json(res.data);
+    })
+    .catch((e) => {
+      response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
