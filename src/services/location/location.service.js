07ab0cf2b59a62beba46a40dea185fe6ba0b700a
Joël Thierry <ntebej@gmail.com>
2021-11-14 01:01:41 +0000

    Feat - Configure firebase functions and move location mock to firebase functions

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,19 +1,14 @@
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
-
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-
-    if (!locationMock) reject("Not found");
-
-    resolve(camelize(locationMock));
-  });
+  return fetch(
+    `http://localhost:5001/mealstogo-8109e/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => res.json());
 };
 
 export const locationTransform = (result) => {
-  const { geometry = {} } = result.results[0];
+  const formattedResponse = camelize(result);
+  const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
 
   return { lat, lng, viewport: geometry.viewport };
