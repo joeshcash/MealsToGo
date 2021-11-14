bffb4832e5abb7a13e382f8ba7697f81e82191a9
Joël Thierry <ntebej@gmail.com>
2021-11-14 03:48:12 +0000

    Feat - Integrating Places photos

--- functions/places/index.js
+++ functions/places/index.js
@@ -3,6 +3,26 @@ const url = require("url");
 
 const { mocks, addMockImage } = require("./mock");
 
+const addGoogleImage = (restaurant) => {
+  const ref = restaurant.photos[0].photo_reference;
+
+  if (!ref) {
+    restaurant.photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/04/mae-mu-pancakes-600x750.jpg",
+    ];
+
+    return restaurant;
+  }
+
+  restaurant.photos = [
+    `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${
+      functions.config().geocodeplaces.key
+    }`,
+  ];
+
+  return restaurant;
+};
+
 module.exports.placesRequest = (request, response, client) => {
   const { location, mock } = url.parse(request.url, true).query;
 
@@ -24,10 +44,10 @@ module.exports.placesRequest = (request, response, client) => {
         type: "restaurant",
         key: functions.config().geocodeplaces.key,
       },
-      timeout: 1000,
+      timeout: 5000,
     })
     .then((res) => {
-      res.data.results = res.data.results.map(addMockImage);
+      res.data.results = res.data.results.map(addGoogleImage);
 
       return response.json(res.data);
     })
