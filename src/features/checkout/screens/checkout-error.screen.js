f8608f48a486131598694c6acc71125621fbd785
Joël Thierry <ntebej@gmail.com>
2021-12-07 17:00:24 +0000

    Fix - Fix bug on checkout error screen

--- src/features/checkout/screens/checkout-error.screen.js
+++ src/features/checkout/screens/checkout-error.screen.js
@@ -4,15 +4,18 @@ import { colors } from "../../../theme/colors";
 
 import SafeArea from "../../../components/utils/SafeArea.component";
 import Text from "../../../components/Typography/Text.component";
+import Spacer from "../../../components/Spacer/Spacer.component";
+
 import { CartIcon, CartIconContainer } from "../checkout.styles";
 
 const CheckoutErrorScreen = ({ route }) => {
-  const { error = "" } = route.paramas;
+  const { error = "" } = route.params;
 
   return (
     <SafeArea>
       <CartIconContainer>
         <CartIcon icon="close" bg={colors.ui.error} />
+        <Spacer position="top" size="large" />
         <Text variant="label">{error}</Text>
       </CartIconContainer>
     </SafeArea>
--- src/features/checkout/screens/checkout-success.screen.js
+++ src/features/checkout/screens/checkout-success.screen.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import SafeArea from "../../../components/utils/SafeArea.component";
 import Text from "../../../components/Typography/Text.component";
+import Spacer from "../../../components/Spacer/Spacer.component";
 
 import { CartIcon, CartIconContainer } from "../checkout.styles";
 
@@ -9,6 +10,7 @@ const CheckoutSuccessScreen = () => (
   <SafeArea>
     <CartIconContainer>
       <CartIcon icon="check-bold" />
+      <Spacer position="top" size="large" />
       <Text variant="label">Success!</Text>
     </CartIconContainer>
   </SafeArea>
